320e36d136b1d74107a5c67778e2637048f0ab25
Yixin Jiang <jiangyixin0910@gmail.com>
2019-04-12 22:01:05 +0200

    Update imageList size

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -10,19 +10,20 @@ class ImageCard extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.imageRef.current)
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
 
+     setSpans = () => {
         let height = this.imageRef.current.clientHeight,
-          spans = height%10 === 0 ? height/10 + 1 : Math.ceil(height/10)
+            spans = height%10 === 0 ? height/10 + 1 : Math.ceil(height/10)
 
-        this.setState({ spans })
+         this.setState({ spans })
     }
 
     render() {
-
         return (
             <div style={{gridRowEnd: `span ${this.state.spans}`}}>
-                <img ref={this.imageRef} src={this.props.image} />
+                <img ref={this.imageRef} src={this.props.image} alt="" />
             </div>
         )
     }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -9,11 +9,22 @@ import Photo3 from '../images/photo-3.jpeg'
 import Photo4 from '../images/photo-4.jpeg'
 import Photo5 from '../images/photo-5.jpeg'
 import Photo6 from '../images/photo-6.jpeg'
-
-
+import Photo7 from '../images/photo-7.jpeg'
+import Photo8 from '../images/photo-8.jpeg'
+import Photo9 from '../images/photo-9.jpeg'
 
 const ImageList = (props) => {
-    const imageNames = [Photo1, Photo2, Photo3, Photo4, Photo5, Photo6]
+    const imageNames = [
+        Photo1, 
+        Photo2, 
+        Photo3, 
+        Photo4, 
+        Photo5, 
+        Photo6, 
+        Photo7, 
+        Photo8, 
+        Photo9
+    ]
 
     const images = imageNames.map((image) => {
         return (
